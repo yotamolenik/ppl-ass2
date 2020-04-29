063ab35759a837a3cef7a86d26549a62cd9be614
Unknown <suchy@post.bgu.ac.il>
2020-04-29 13:32:47 +0300

    Fixed

---
+++ test/q4.ts
@@ -0,0 +1,10 @@
+import { Exp, Program} from '../imp/L2-ast';
+import { Result } from '../imp/result';
+
+/*
+Purpose: @TODO
+Signature: @TODO
+Type: @TODO
+*/
+export const l2ToJS = (exp: Exp | Program): Result<string> => 
+    @TODO
