fd69dde8efc07987c7126e0f61d4855bedf534c3
Unknown <suchy@post.bgu.ac.il>
2020-04-29 13:20:56 +0300

    question4

---
+++ test/q3.ts
@@ -0,0 +1,18 @@
+import { ForExp, AppExp, Exp, Program } from "./L21-ast";
+import { Result } from "../imp/result";
+
+/*
+Purpose: @TODO
+Signature: @TODO
+Type: @TODO
+*/
+export const for2app = (exp: ForExp): AppExp =>
+    @TODO
+
+/*
+Purpose: @TODO
+Signature: @TODO
+Type: @TODO
+*/
+export const L21ToL2 = (exp: Exp | Program): Result<Exp | Program> =>
+    @TODO
